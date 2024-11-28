9c38ee6c8e5fd4740a41903f0d91c93d59399172
ranjandvlpr <ranjandvlpr@gmail.com>
2024-11-28 15:14:56 +0530

    upload on github

---
+++ MRP/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from MRP.get_recommendations import get_recommendations
+
+
+def get_movie(request):
+    context = {
+        'message': None,
+        'recommendations': None
+    }
+    if request.method == 'POST':
+        genre = request.POST.get('Genre')
+        certificate = request.POST.get('certificate')
+        rating = request.POST.get('rating')
+        recommendations = get_recommendations(genre, certificate, float(rating))
+
+        if isinstance(recommendations, str):
+            context['message'] = recommendations
+            context['recommendations'] = None
+        else:
+            context['recommendations'] = recommendations
+            context['range'] = range(1, 11)
+
+    return render(request, 'index.html', context)
+
+
+# Create your views here.
